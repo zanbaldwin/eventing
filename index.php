7b15fab277f0e3eb87df7815de9e44d4c52ccb0a
Alexander Baldwin <zanders@zafr.net>
2010-06-17 15:58:54 +0100

    Updated source link in index file.

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
    * @copyright  (c) 2009 Alexander Baldwin
    * @license    http://www.gnu.org/licenses/gpl.txt GNU General Public License
    * @version    v0.4
-   * @link       http://eventing.zafr.net/source/index.php
+   * @link       http://github.com/mynameiszanders/eventing
    * @since      v0.1
    */
 
