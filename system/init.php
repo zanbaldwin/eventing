305da8d89b8834f21e89464925ae1df96c8ca10b
Alexander Baldwin <zanders@zafr.net>
2010-06-21 16:51:35 +0100

    Ammended coding style to adhere to Drupal standards.

--- system/init.php
+++ system/init.php
@@ -116,33 +116,38 @@
   $c['baseurl']   = $c['server'].$c['url'];
 
   // Directory Constants.
-  $c['basepath']  = rtrim(
-                      str_replace('\\', '/', realpath(dirname($main_file))),
-                      '/'
-                    )
-                  . '/';
-  $c['sys']       = rtrim(str_replace('\\', '/', realpath($system_folder)), '/') . '/';
-  $c['app']       = rtrim(str_replace('\\', '/', realpath($default_app)), '/') . '/';
-  $c['contentpath']   = rtrim(str_replace('\\', '/', realpath($content_folder)), '/') . '/';
-  // Check that the content directory is a sub-directory of the web root. If it is not, set it as null.
+  $c['basepath'] = rtrim(
+                     str_replace('\\', '/', realpath(dirname($main_file))),
+                     '/'
+                   )
+                 . '/';
+  $c['sys'] = rtrim(str_replace('\\', '/', realpath($system_folder)), '/') . '/';
+  $c['app'] = rtrim(str_replace('\\', '/', realpath($default_app)), '/') . '/';
+  $c['contentpath'] = rtrim(
+                        str_replace('\\', '/', realpath($content_folder)), '/'
+                      )
+                    . '/';
+  // Check that the content directory is a sub-directory of the web root. If it
+  // is not, set it as null.
   $c['content'] = null;
-  if(is_string($_SERVER['DOCUMENT_ROOT']))
-  {
+  if (is_string($_SERVER['DOCUMENT_ROOT'])) {
     $len = strlen($_SERVER['DOCUMENT_ROOT']);
-    if(substr($c['contentpath'], 0, $len) == $_SERVER['DOCUMENT_ROOT'])
-    {
-      $c['content'] = $c['server'] . '/' . trim(substr($c['contentpath'], $len), '/') . '/';
+    if (substr($c['contentpath'], 0, $len) == $_SERVER['DOCUMENT_ROOT']) {
+      $c['content'] = $c['server']
+                    . '/'
+                    . trim(substr($c['contentpath'], $len), '/')
+                    . '/';
     }
   }
-  // If the contenturl cannot be established, or it is outside the web root, there is no point having the content path.
-  if(is_null($c['content']))
-  {
+  // If the contenturl cannot be established, or it is outside the web root,
+  // there is no point having the content path.
+  if (is_null($c['content'])) {
     $c['contentpath'] = null;
   }
 
-  // All our constants are really great, but they're a little soft at the moment... Shall we make them hardcore?
-  foreach($c as $name => $const)
-  {
+  // All our constants are really great, but they're a little soft at the
+  // moment... Shall we make them hardcore?
+  foreach ($c as $name => $const) {
     $name = strtoupper($name);
     defined($name) || define($name, $const);
   }
@@ -153,49 +158,53 @@
     $content_folder, $skeleton_mode, $config_type, $c, $name, $const
   );
 
-    // Right, we have all out constants defined, with no loose variables floating about... I think we're doing pretty
-    // well! Shall we load some common functions? Let's!
+  // Right, we have all out constants defined, with no loose variables floating
+  // about... I think we're doing pretty well! Shall we load some common
+  // functions? Let's!
 
-    $common = SYS . 'common' . EXT;
-    file_exists($common) || E_Core_Error('Common functions file does not exist.', __FILE__, __LINE__);
-    require_once $common;
+  $common = SYS . 'common' . EXT;
+  file_exists($common)
+    || E_Core_Error('Common functions file does not exist.', __FILE__, __LINE__);
+  require_once $common;
 
-    // Cool. We have functions. Now we want libraries! Big, fat juicy ones first, for functionality. Then we can have
-    // the lean, mean, big-boss libraries!
-    // To make it simple: URI, Router, Core, Controller and Model libraries...
-    $URI =& load_class('uri');
-    $RTR =& load_class('router');
-    load_class('core', false);
-    load_class('controller', false);
-    load_class('model', false);
+  // Cool. We have functions. Now we want libraries! Big, fat juicy ones first,
+  // for functionality. Then we can have the lean, mean, big-boss libraries! To
+  // make it simple: URI, Router, Core, Controller and Model libraries...
+  $URI =& load_class('uri');
+  $RTR =& load_class('router');
+  load_class('core', false);
+  load_class('controller', false);
+  load_class('model', false);
 
-    // We want to know what request this application is meant to serve!
-    if(!is_array($dcm = $RTR->dcm()))
-    {
-        show_404();
-    }
+  // We want to know what request this application is meant to serve!
+  if (!is_array($dcm = $RTR->dcm())) {
+    show_404();
+  }
 
-    // Directory should come in format 'path/to/controller/', with a trailing slash.
-    // Make an absolute path to the controller file, and include it.
-    $controller_file = APP . 'controllers/' . $dcm[0] . $dcm[1] . EXT;
-    // Check that the file provided by $dcm exists, because the router library may not of used the recursive method.
-    file_exists($controller_file) || show_404();
-    require_once $controller_file;
+  // Directory should come in format 'path/to/controller/', with a trailing
+  // slash. Make an absolute path to the controller file, and include it.
+  $controller_file = APP . 'controllers/' . $dcm[0] . $dcm[1] . EXT;
+  // Check that the file provided by $dcm exists, because the router library may
+  // not of used the recursive method.
+  file_exists($controller_file) || show_404();
+  require_once $controller_file;
 
-    // Strip all extensions. We only want the name of the controller class now!
-    if(($pos = strpos($dcm[1], '.')) !== false)
-    {
-        $dcm[1] = reset(explode('.', $dcm[1]));
-    }
+  // Strip all extensions. We only want the name of the controller class now!
+  if (($pos = strpos($dcm[1], '.')) !== false) {
+    $dcm[1] = reset(explode('.', $dcm[1]));
+  }
 
-    // Make sure the controller class exists.
-    class_exists($dcm[1]) || show_404();
-    $controller = new $dcm[1];
-    // Make sure the method function exists.
-    method_exists($controller, $dcm[2]) || in_array($dcm[2], get_class_methods($dcm[1]), true) || show_404();
-    $controller->$dcm[2]();
+  // Make sure the controller class exists.
+  class_exists($dcm[1]) || show_404();
+  $controller = new $dcm[1];
+  // Make sure the method function exists.
+  method_exists($controller, $dcm[2])
+    || in_array($dcm[2], get_class_methods($dcm[1]), true)
+    || show_404();
+  $controller->$dcm[2]();
 
-    // Right, that's everything done! Just dump the output to the client end finish the script!
-    $E =& get_instance();
-    $E->output->display();
+  // Right, that's everything done! Just dump the output to the client end
+  // finish the script!
+  $E =& get_instance();
+  $E->output->display();
 
