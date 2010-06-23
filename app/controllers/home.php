a1fb3362dce8a205b4dcf1b34162e01c52981590
Alexander Baldwin <zanders@zafr.net>
2010-06-23 14:02:04 +0100

    Created new page in default controller to show advanced example usage of template library and new HTML5 theme.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -30,4 +30,47 @@
       $this->template->load('shell');
     }
 
+    public function html5()
+    {
+      $this->load->library('template');
+
+      // Set the theme folder we have saved all these in.
+      $this->template->set_theme('html5');
+      
+      // Load the views into objects that we can use.
+      $this->template->create(array(
+        // Structure
+        'shell' => 'xhtml5shell',
+        'style',
+        'script',
+        'header',
+        'nav',
+        'content',
+        'footer',
+      
+        // Content
+        'node_1' => 'node',
+        'node_2' => 'node'
+      ));
+      // Add data to the
+      $this->template->section('shell')->add(array(
+        'title' => 'HTML5 Eventing Template'
+      ));
+      $this->template->section('nav')->add(array(
+        // List of links. The format has yet to be decided.
+        'primary' => array()
+      ));
+      // Create a pseudo-section. Not a section on it's own, but a collection of
+      // other sections.
+      $this->template->combine('posts', array('node_1', 'node_2'));
+      $links = array(
+        'shell' => array('style', 'script', 'header', 'content', 'sidebar', 'footer'),
+        'header' => array('nav'),
+        'content' => array('posts'),
+      );
+      // Symbolically link the views together and load the parent.
+      $this->template->link($links);
+      $this->template->load('shell');
+    }
+    
   }
