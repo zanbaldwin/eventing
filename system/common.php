a29a595032130be90f512337adfcec17fb306c44
Alexander Baldwin <zanders@zafr.net>
2010-09-11 15:49:31 +0100

    Completed show_error() function. Now tries to include an error document, and falls back to an XML document to display data.

--- system/common.php
+++ system/common.php
@@ -313,18 +313,44 @@ if(!function_exists('show_error'))
    *
    * @return exit
    */
-  function show_error($msg, $header = '500 Framework Application Error', $format = '') {
-    if(!headers_sent()) {
-    	if(preg_match('|^[0-9]{3} |', $header, $matches)) {
-    		$code = (int) $matches[1];
-    		if($code < 600 && $code >= 100) {
-    			header($header, true, $code);
-    		}
+  function show_error($msg, $header = '500 Framework Application Error') {
+    if(is_string($header)
+       && preg_match('|^([0-9]{3}) |', $header, $matches)
+       && (int) $matches[1] < 600
+       && (int) $matches[1] >= 100
+    ) {
+    	if(!headers_sent()) {
+    			header($header, true, (int) $matches[1]);
     	}
     }
-    // Just just the message right now, don't have time to write anything. Those
-    // shirts aren't going to iron themselves!
-    exit($msg);
+    else {
+    	$header = '500 Framework Application Error';
+    }
+    // Grab some information from the backtrace. It might be useful.
+    $trace = debug_backtrace();
+    $error = array(
+      'message' => $msg,
+      'title' => $header,
+      'status' => (int) $matches[1],
+      'file' => $trace[0]['file'],
+      'line' => $trace[0]['line']
+    );
+    // Unset any variables that we don't want included in the error document.
+    unset($msg, $header, $matches, $trace);
+    if(file_exists(theme_path('errors') . 'error' . EXT)) {
+    	// We are writing about the path twice because we don't want to set
+    	// anymore variables.
+    	require theme_path('errors') . 'error' . EXT;
+    }
+    else {
+    	// No HTML document to show? Dump out the data in an XML document instead.
+    	echo '<?xml version="1.0" encoding="utf-8" ?>' . "\n<error>\n";
+    	foreach($error as $element => $value) {
+    		echo "  <{$element}>\n    {$value}</{$element}>\n";
+    	}
+    	echo '</error>';
+    }
+    exit;
   }
 }
 
