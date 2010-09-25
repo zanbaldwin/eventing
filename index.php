9ea6747562c935f291cc124d2796fd6a5895ac5a
Alexander Baldwin <zanders@zafr.net>
2010-09-25 15:03:10 +0100

    Formatting.

--- index.php
+++ index.php
@@ -23,50 +23,51 @@
  * @since      v0.1
  */
 
-# ========================================================================== #
-# Please edit the following settings depending on your folder structure and  #
-# preferences. If you are going to keep the default values here, you might   #
-# as well remove this array. All other editable system settings can be found #
-# in "<system_folder>/config/".                                              #
-# ========================================================================== #
-
-$user_config = array(
-    'system_folder'  => 'system',
-    'default_app'    => 'app',
-    'content_folder' => 'public',
-# Can be "ini" or "array". Defaults to "array".
-    'config_type'    => 'array'
-    );
-
-    # ========================================================================== #
-    # End of user configurable settings.                                         #
-    # ========================================================================== #
-
-    // Killing two birds with one stone. Define a constant for other files to
-    // check that they're not being called independently, and to set the core
-    // benchmark. The E_FRAMEWORK constant is REQUIRED!
-    defined('E_FRAMEWORK') || define('E_FRAMEWORK', microtime(true));
-
-    // However, the version number doesn't really have any use in the framework at
-    // all. You may discard it if you want. I'm not very good at versions. I never
-    // remember to update the version number, so I'm just going to stick with
-    // "pre" or "post" public releases.
-    defined('E_VERSION') || define('E_VERSION', 'Alpha1-PrePublicRelease');
-
-    // Right! Let's get this party started!
-    $init = isset($user_config['system_folder'])
-    ? $user_config['system_folder'] . '/init.php'
-    : 'system/init.php';
-    $init = realpath($init);
-
-    // For obvious reasons, let's check if the initialisation script exists...
-    // If it doesn't, we'll give the user a big slap in the face of epic
-    // disappointment by calling the exit function. SAD TIMES!
-    file_exists($init) || exit('Initialisation script not found.');
-    // Great... Everything working so far. What a boring life we live!
-    // Don't forget to say which file is calling the script.
-    // TODO: Should this be a constant?
-    //       defined('E_MAIN_FILE') || define('E_MAIN_FILE', __FILE__);
-    $main_file = __FILE__;
-    require_once $init;
+	# ========================================================================== #
+	# Please edit the following settings depending on your folder structure and  #
+	# preferences. If you are going to keep the default values here, you might   #
+	# as well remove this array. All other editable system settings can be found #
+	# in "<system_folder>/config/".                                              #
+	# ========================================================================== #
+	
+	$user_config = array(
+	  'system_folder'  => 'system',
+	  'default_app'    => 'app',
+	  'content_folder' => 'public',
+	  # Can be "ini" or "array". Defaults to "array".
+	  'config_type'    => 'array'
+	);
+	
+	# ========================================================================== #
+	# End of user configurable settings.                                         #
+	# ========================================================================== #
+	
+	// Killing two birds with one stone. Define a constant for other files to
+	// check that they're not being called independently, and to set the core
+	// benchmark. The E_FRAMEWORK constant is REQUIRED!
+	defined('E_FRAMEWORK') || define('E_FRAMEWORK', microtime(true));
+	
+	// However, the version number doesn't really have any use in the framework at
+	// all. You may discard it if you want. I'm not very good at versions. I never
+	// remember to update the version number, so I'm just going to stick with
+	// "pre" or "post" public releases.
+	defined('E_VERSION') || define('E_VERSION', 'Alpha1-PrePublicRelease');
+	
+	// Right! Let's get this party started!
+	$init = isset($user_config['system_folder'])
+	      ? $user_config['system_folder'] . '/init.php'
+	      : 'system/init.php';
+	$init = realpath($init);
+	
+	// For obvious reasons, let's check if the initialisation script exists...
+	// If it doesn't, we'll give the user a big slap in the face of epic
+	// disappointment by calling the exit function. SAD TIMES!
+	file_exists($init) || exit('Initialisation script not found.');
+	// Great... Everything working so far. What a boring life we live!
+	// Don't forget to say which file is calling the script.
+	// We are not going to define this as a constant because we can just use:
+	// $main_file = BASEPATH . SELF;
+	// to get it again.
+	$main_file = __FILE__;
+	require_once $init;
 
