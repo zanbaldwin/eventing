64f5635b15ef5fd103098091a9a627a42320bf0a
Alexander Baldwin <zanders@zafr.net>
2010-11-17 09:28:18 +0000

    Added Module library.

---
+++ system/libraries/module.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Eventing Framework Module Library
+ *
+ * Eventing PHP Framework by Alexander Baldwin (zanders [at] zafr [dot] net).
+ * http://eventing.zafr.net/
+ * The Eventing Framework is an object-orientated PHP Framework, designed to
+ * rapidly build applications. This is where we start all our settings,
+ * libraries and other odd-jobs to get the ball rolling...
+ *
+ * @category   Eventing
+ * @package    Libraries
+ * @subpackage module
+ * @copyright  (c) 2009 Alexander Baldwin
+ * @license    http://www.gnu.org/licenses/gpl.txt - GNU General Public License
+ * @version    v0.4
+ * @link       http://github.com/mynameiszanders/eventing
+ * @since      v0.1
+ */
+
+  namespace Eventing\Library;
+
+  if(!defined('E_FRAMEWORK')) {
+    headers_sent() || header('HTTP/1.1 404 Not Found', true, 404);
+    exit('Direct script access is disallowed.');
+  }
+
+  /**
+   * Module Library
+   * This library does NOT extend the library class, as multiple instances are
+   * required.
+   */
+  class module extends library {
+
+    protected function __construct() {}
+
+  }
