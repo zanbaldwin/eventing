99b174cac4821a4cdbfa294f56d71d4e8fc799d3
Alexander Baldwin <zanders@zafr.net>
2010-11-18 15:36:21 +0000

    Re-jiggle of the configuration files.

--- app/config/config.php
+++ app/config/config.php
@@ -20,66 +20,63 @@
  * @since      v0.1
  */
 
-if (!defined('E_FRAMEWORK')) {
-  headers_sent() || header('HTTP/1.1 404 Not Found', true, 404);
-  exit('Direct script access is disallowed.');
-}
+  if (!defined('E_FRAMEWORK')) {
+    headers_sent() || header('HTTP/1.1 404 Not Found', true, 404);
+    exit('Direct script access is disallowed.');
+  }
 
-$config = array(
+  $config = array(
 
-// Generic System Properties:
+    // Generic System Properties:
     'default_charset' => 'utf-8',
     'encryption_key' => 'lolomgwtf',
     'default_timezone' => 'GMT',
     'error_types_trigger' => 17234,
 
-// Date Preferences:
+    // Date Preferences:
     'time_short' => 'G:i',
     'date_short' => 'j/n/y',
     'time_long' => 'g:i a',
     'date_long' => 'l jS, F Y',
 
-// Load::View and Template Class Settings:
+    // Load::View and Template Class Settings:
     'default_theme' => 'default',
 
-// Database Preferences:
+    // Database Preferences:
     'db_host' => 'localhost',
     'db_user' => 'eventing_app',
     'db_pass' => 'eventing_iscoollike',
     'db_name' => 'eventing_db',
 
-// URI Class Settings:
-// Don't bother including a foward slash ('/'):
-    'url_default_suffix' => 'htm',
-
-// HTTP Digest Authentication
+    // HTTP Digest Authentication
     'default_realm' => 'Eventing',
 
-// For the anchor - a() - function:
+    // For the anchor - a() - function.
     'redirect_refresh' => false,
     'save_gets' => array('l'),
     'cookie_name' => 'E_cookie',
 
-// Input Class Settings:
-// '^(get|post):[a-zA-Z0-9_-]+$'
+    // Input Class Settings:
+    // '^(get|post):[a-zA-Z0-9_-]+$'
     'xss_filter_bypass' => 'post:_some_super-duper_unobvious_key',
     'xss_replace_text' => '[xss-removed]',
     'xss_allowed_html' => array(
       'strong', 'b', 'em', 'i', 'h2', 'h3', 'h4', 'p', 'span', 'img', 'code',
       'blockquote', 'q', 'hr'
-      ),
+    ),
 
-      // Router Class Settings:
+    // Router Class Settings:
+    'default_suffix' => '/',
     'file_over_folder' => false,
     'default_controller' => 'home',
     'default_method' => 'index',
 
-      // To enable usage of Zend Libraries within the framework, set this to where
-      // the Zend Library folder is located. For example, if the Zend Loader class
-      // is located at "/home/username/public_html/Zend/Loader.php", set this to
-      // "/home/username/public_html". This does not need to be absolute, just set
-      // relative to the $main_file. By default, this is set to the SYS path.
+    // To enable usage of Zend Libraries within the framework, set this to where
+    // the Zend Library folder is located. For example, if the Zend Loader class
+    // is located at "/home/username/public_html/Zend/Loader.php", set this to
+    // "/home/username/public_html". This does not need to be absolute, just set
+    // relative to the $main_file. By default, this is set to the SYS path.
     'zend_container_path' => SYS,
-    'zend_autoload' => true
+    'zend_autoload' => true,
 
-      );
+  );
