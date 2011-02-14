930e3b37d468e7385b6f6b28689fbdcc7892fe57
Alexander Baldwin <zanders@zafr.net>
2011-02-14 14:28:39 +0000

    Rewrite the landing page to something so much simpler.

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -36,128 +36,13 @@
     }
 
     public function index() {
-      
       $this->load->library('template');
-      // Compile a list of routes to test.
-      $routes = array(
-        'example@mycontroller',
-        '',
-        '/',
-        'home',
-        'home/',
-        'home/index/',
-
-        'controller',
-        '/controller',
-
-        'welcome/home',
-        'welcome/home/',
-
-        'controller/',
-        '/controller/',
-        
-        'controller/method',
-        '/controller/method',
-        
-        'controller.suffix',
-        '/controller.suffix',
-        
-        'controller/method.suffix',
-        '/controller/method.suffix',
-        
-        'controller/method.suffix/',
-        '/controller/method.suffix/',
-        
-        'module@controller/method.suffix',
-        'module@/controller/method.suffix',
-        
-        'module@',
-        'module@/',
-        
-        'module@.suffix',
-        'module@/.suffix',
-        
-        '/.suffix',
-        '.suffix',
-        
-        '?inline=http_q_array',
-        '/?inline=http_q_array',
-        
-        '?query?',
-        '/?query?',
-        
-        '?query',
-        '/?query',
-      
-        '??',
-        '/??',
-        
-        '#fragment',
-        '/#fragment',
-      
-        '#',
-        '/#',
-        
-        'module@/controller/method/param.suffix?query=param#fragment',
-        'module @ /controller/method/param .suffix ?query=param #fragment',
-        
-        '/?prowl',
-      );
-
-      // Success Rates
-      $anchor_success = 0;
-      $router_success = 0;
-      // Echo out all the route tests.
-      $data = array();
-      $invalid = content('images/slash.png');
-      $invalid = $invalid
-               ? '<img src="' . $invalid . '" alt="Invalid" width="16" height="16" />'
-               . ' <span style="color:#900;">Invalid</span>'
-               : '';
-      $valid = content('images/tick.png');
-      $valid = $valid
-             ? '<img src="' . $valid . '" alt="Invalid" width="16" height="16" />'
-             : '';
-      if(is_array($routes) && $routes) {
-        foreach($routes as $route) {
-          $r = $this->router->route($route);
-          $rvalid = is_object($r) && $r->valid;
-          $temp = array(
-            'euri'  => $route,
-          );
-          $example_options = array('query' => array('action' => 'do'));
-          if($a = a($route, htmlentities(a($route, false, $example_options)), $example_options)) {
-            $temp['url'] = $valid . ' ' . $a;
-            $anchor_success++;
-          }
-          else {
-            $temp['url'] = $invalid;
-          }
-          if($rvalid) {
-            $temp['controller'] = $valid . ' ' . $r->controller() . '::' . $r->method();
-            if($r->suffix() != '/') {
-              $temp['controller'] .= '(<span>'.$r->suffix().'</span>)';
-            }
-            $temp['file'] = $valid . ' ' . str_replace(BASEPATH, '', $r->path());
-            $router_success++;
-          }
-          else {
-            $temp['controller'] = $invalid;
-            $temp['file'] = $invalid;
-          }
-          $data[] = (object) $temp;
-        }
-      }
-      $this->template->create(array('s' => 'html5shell', 'content'));
-      $this->template->section('s')->add(array(
-        'title'   => 'Eventing PHP Application Framework',
-        'heading' => __METHOD__ . '(<span>' . $this->router->suffix() . '</span>)',
+      $this->template->create(array(
+        'shell' => 'welcome_message',
+        'head' => 'head',
       ));
-      $this->template->section('content')->add(array(
-        'routes'  => $data,
-      ));
-      $this->template->link(array('s' => array('content')));
-      $this->template->load('s');
+      $this->template->section('shell')->add('title', 'Eventing PHP Framework');
+      $this->template->load('shell');
     }
 
   }
---
+++ app/themes/default/welcome_message.php
@@ -0,0 +1,26 @@
+<?php
+  echo "<?xml version=\"1.0\" encoding=\"utf-8\" />\n";
+?><!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+  <!--{head}-->
+
+  <body>
+    <div class="container">
+
+      <h1>Eventing</h1>
+      <p>Welcome to the Eventing PHP Application Framework.</p>
+
+      <p>The page you are looking at is being generated dynamically. If you would like to edit this page, you'll find it located at:</p>
+      <code>app/themes/default/welcome_message.php</code>
+      
+      <p>The corresponding controller for this page is located at:</p>
+      <code>app/controllers/home.php</code>
+      
+      <p>Unfortunately, documentation is rather lacking at the moment. This will be rectified by the time we get round to releasing a public version (still under development).</p>
+      <p>Page rendered in {elapsed_time} seconds.</p>
+
+    </div>
+  </body>
+
+</html>
