73abdc51c2b526ba48d1eb77dcfb5af3f6857e53
Alexander Baldwin <zanders@zafr.net>
2011-02-14 15:06:36 +0000

    Added missing quotation marks. Used $title variable, rather than a hard-coded string.

--- app/themes/default/welcome_message.php
+++ app/themes/default/welcome_message.php
@@ -5,7 +5,7 @@
 
   <head>
     <meta charset="utf-8" />
-    <title>Skype Jobs</title>
+    <title><?php echo $title; ?></title>
     <meta name="description" content="Eventing PHP Application Framework" />
     <!-- The favourites icon must be in the web root (for IE, surprise surprise). -->
     <link href="<?php echo content('css/main.css'); ?>" rel="stylesheet" media="screen, projection" type="text/css" title="Stylesheet" />
@@ -13,13 +13,13 @@
         // Define external scripts to be loaded.
         var s = [
           "https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js",
-          "<?php echo content('js/main.js'); ?>,
+          "<?php echo content('js/main.js'); ?>",
         ];
         // Load HeadCatch <http://github.com/mynameiszanders/headcatch>.
         (function() {
           var h = d.createElement("script"),
               t = d.getElementsByTagName("script")[0];
-          h.src = "<?php echo content('js/head.catch.js'); ?>;
+          h.src = "<?php echo content('js/head.catch.js'); ?>";
           h.async = true;
           t.parentNode.insertBefore(h, t);
         })();
