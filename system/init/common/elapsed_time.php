95c17eae636bbdfff70767be0f2d778363f74570
Zander Baldwin <mynameiszanders@gmail.com>
2011-10-02 14:13:29 +0100

    No need to check if the end time is a floating-point number.

--- system/init/common/elapsed_time.php
+++ system/init/common/elapsed_time.php
@@ -9,7 +9,7 @@
  * @see			/index.php
  */
 
-	if (!defined('E_FRAMEWORK')) {
+	if(!defined('E_FRAMEWORK')) {
 		headers_sent() || header('HTTP/1.1 404 Not Found', true, 404);
 		exit('Direct script access is disallowed.');
 	}
@@ -18,9 +18,8 @@
 		/**
 		 * Elapsed Time
 		 *
-		 * Return the elapsed time in seconds, between the time specified time
-		 * passed to the function (must be the return of the function microtime)
-		 * and now.
+		 * Return the elapsed time in seconds, between the time specified time passed to the function (must be the
+		 * return of the function microtime) and now.
 		 *
 		 * @param  string|float $start
 		 * @return false|float
@@ -30,12 +29,13 @@
 			$end = microtime(true);
 			// The user probably passed the microtime as a string.
 			$regex = '/^0\\.([0-9]+) ([0-9]+)$/';
-			if (is_string($start)) {
+			if(is_string($start)) {
 				$start = preg_match($regex, $start)
 					? (float) preg_replace($regex, '$2.$1', $start)
 					: false;
 			}
-			if (!is_float($start) || !is_float($end)) {
+			// If the start time isn't a floating-point number, then we can't exactly do any calculations on it, can we?
+			if(!is_float($start)) {
 				return false;
 			}
 			$elapsed_time = round($end - $start, 3);
