a6c8d68bcd07d5be5d69631714d6342d08204639
Zander Baldwin <mynameiszanders@gmail.com>
2011-10-03 00:11:31 +0100

    Rewrite the load_class() function to take into account libraries that may reside in the system directory, the application directory, or both.

--- system/init/common/load_class.php
+++ system/init/common/load_class.php
@@ -18,97 +18,108 @@
 		/**
 		 * Load Class
 		 *
-		 * For loading classes/libraries. Objects in PHP are returned by reference
-		 * anyway, so we don't need to declare that part. This also saves on the
-		 * amount of strict (2048) warnings when returning a boolean (eg. the
-		 * library doesn't exist).
+		 * For loading classes/libraries. Objects in PHP are returned by reference anyway, so we don't need to declare
+		 * that part. This also saves on the amount of strict (2048) warnings when returning a boolean (eg. the library
+		 * doesn't exist).
+		 * The second parameter specifies whether to return an instance of the library. Some libraries, such as library,
+		 * controller, module and model must not be initiated on their own. WARNING: Once a library has been loaded with
+		 * either boolean flag, it cannot be changed in the process of the application. It is hard-coded.
 		 *
-		 * @param string $lib
-		 * @param bool $return
+		 * @param string $identifier
+		 * @param bool $instance
 		 * @return boolean|object
 		 */
-		function load_class($identifier, $return = true) {
-			// Create a static array to house our loaded files in - we don't want to
-			// include a class definition twice now, do we?
-			static $files = array();
-			// No point continuing with the function if no library has been specified.
+		function load_class($identifier, $instance = true) {
+			// Create a static array to house our loaded files in - we don't want to include a class definition twice now, do we?
+			static $instances = array();
+			// No point continuing with the rest of the function if no library has been specified.
 			if(!is_string($identifier)) {
 				return false;
 			}
-			// Check if the specified library is an empty string and that it adheres
-			// to the "module:path/to/library" syntax.
-			$regex = '#^(([a-zA-Z_][a-zA-Z0-9_]*)?@)?([a-zA-Z_][a-zA-Z0-9_/]*)$#';
+			// Strip away unwanted characters on the whole string before it gets complicated and split up.
+			$identifier = strtolower(
+				trim(
+					str_replace(
+						'@/',
+						'@',
+						filter_path($identifier)
+					),
+					'/'
+				)
+			);
+			// There is also no point continuing if the string is empty, or that it contains an invalid string.
+			// For a valid string, it must adhere to "module@path/to/library".
+			$regex = '/^(([a-zA-Z][a-zA-Z0-9_]*)@)?([a-zA-Z_][a-zA-Z0-9_\\/]*)$/';
 			if(!$identifier || !preg_match($regex, $identifier, $parts)) {
 				return false;
 			}
-			// Assign our parts to something a little more human-readable.
-			$module = $parts[2] ? strtolower($parts[2]) : false;
-			// Filter our library string to all lowercase, no separators on the ends
-			// of the string or doubled up.
-			$lib = trim(filter_path(strtolower($parts[3])), '/');
-			// Since the identifier string check out? Let's rebuild it so that the
-			// string will be identical to other calls to this function.
-			$identifier = $module ? $module . ':' . $lib : $lib;
-			// Now that we have a usable identifier string, the only useful
-			// extractable information in the library string is the class name. Make
-			// sure xplode() and end() are called as two separate statements as to
-			// avoid an E_STRICT error.
-			$class = xplode('/', $lib);
+			// Transfer the split pieces of the identifier string into the human-readable module and library path
+			// variables.
+			$module = $parts[2] ? $parts[2] : null;
+			$library_path = $parts[3];
+			// Rebuild the identifier string so the library has a unqiue identifier string, regardless of varying user
+			// input.
+			$identifier = $module . '@' . $library_path;
+			// Now we have the identifier, check if we have already loaded this library?
+			if(isset($instances[$identifier])) {
+				// Just return the value regardless, if loading was successful before, the value will be the libraries
+				// singleton instance. If not, then the value will just be a boolean false.
+				return $instances[$identifier];
+			}
+			// Are we still going? We haven't loaded it before, and must do more work! Sigh!
+			// Grab the name of the class we will be loading. NB: Combining the xplode() and end() functions into one
+			// statement will cause an E_STRICT error.
+			$class = xplode('/', $library_path);
 			$class = end($class);
-			// Transform the library name to an absolute namespace and class
-			// reference.
-			$class = $module
-				? ns(NS, NSMODULE, $module, NSLIBRARY) . $class
-				: ns(NS, NSLIBRARY)                    . $class;
-			// If we have already loaded the file in question, then return the
-			// appropriate - we do not want to load the file again.
-			if(isset($files[$identifier])) {
-				// If load_class() failed the first time, then it's not going to work a
-				// second time, is it?
-				if(!$files[$identifier]) {
-					return false;
+			// Define the application and system paths and class names.
+			$library_path = '/libraries/' . $library_path . EXT;
+			$file = array(
+				'app' => $module
+					? MOD . $module . $library_path
+					: APP . $library_path,
+				'sys' => $module
+					? null
+					: SYS . $library_path,
+			);
+			$class = array(
+				'app' => $module
+					? ns(NS, NSMODULE, $module, NSLIBRARY) . $class
+					: ns(NS, NSCONTROLLER, NSLIBRARY) . $class,
+				'sys' => $module
+					? null
+					: ns(NS, NSLIBRARY) . $class,
+			);
+			// Include the system library, and then the application library file after, so that it can extend the
+			// system library.
+			foreach(array('sys', 'app') as $loc) {
+				if(file_exists($file[$loc])) {
+					require_once $file[$loc];
+					if(!class_exists($class[$loc])) {
+						$class[$loc] = false;
+					}
 				}
-				// load_class() was successful last time! Now, did the function callee
-				// want an instance returned?
-				return $return ? $class::getInstance() : true;
+				else {
+					$class[$loc] = false;
+				}
 			}
-			// This must be the first call this function specifying this library.
-			// Determine the file path, and include the file.
-			$file = $module
-				? MOD . $module . '/libraries/' . $lib . EXT
-				: SYS . 'libraries/' . $lib . EXT;
-			// If the file exists, return false, remembering to set the $files array
-			// first, so that we can save ourselves the trouble of querying the
-			// filesystem for every call to this library.
-			if(!file_exists($file)) {
-				$files[$identifier] = false;
-				return false;
+			// Ideally, we want the application library, but if we can't have that we'll fall back to the system
+			// library. Obviously if no library exists, we'll just return a boolean false.
+			switch(true) {
+				case $class['app']:
+					$instances[$identifier] = $instance
+						? $class['app']::getInstance()
+						: true;
+					break;
+				case $class['sys']:
+					$instances[$identifier] = $instance
+						? $class['sys']:: getInstance()
+						: true;
+					break;
+				default:
+					$instances[$identifier] = false;
+					break;
 			}
-			// Whack it in, baby!
-			// This is where we hope the file actually doesn't contain anything to
-			// screw up the framework, like inline HTML, or worse,
-			require_once $file;
-			// Check that the library class exists.
-			if(!class_exists($class)) {
-				$files[$identifier] = false;
-				return false;
-			}
-			// If the function callee has specified that they do not want an instance
-			// returned, just return true here.
-			// Do NOT add anything to the $files array, because the library they
-			// specified may not implement the getInstance() method!
-			if(!$return) {
-				return true;
-			}
-			if(!method_exists($class, 'getInstance')) {
-				$files[$identifier] = false;
-				return false;
-			}
-			// Well done! We have determined the library, the file it is contained in,
-			// that the class exists, and that it implements a method called
-			// getInstance()!
-			//Add a true boolean to the $files array, and return an instance.
-			$files[$identifier] = true;
-			return $class::getInstance();
+			return $instances[$identifier];
+			
 		}
 	}
