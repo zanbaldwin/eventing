a76e22952c93129660256950186b38e858afe7ba
Zander Baldwin <mynameiszanders@gmail.com>
2011-10-02 14:35:26 +0100

    Whoops, deleted the end character from the IF conditional statement instead of the trailing whitespace.

--- system/init/constants.php
+++ system/init/constants.php
@@ -11,7 +11,7 @@
  * @see			/index.php
  */
 
-	i (!defined('E_FRAMEWORK')) {
+	if(!defined('E_FRAMEWORK')) {
 		headers_sent() || header('HTTP/1.1 404 Not Found', true, 404);
 		exit('Direct script access is disallowed.');
 	}
