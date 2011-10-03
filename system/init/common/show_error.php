880b9b19d98cbb7a4a76e4b8114783bd843013d1
Zander Baldwin <mynameiszanders@gmail.com>
2011-10-03 20:37:27 +0100

    Combined the show_doc() function into the show_error() function, and changed the parameters required. Now just needs a $message and $header; these have swapped round so that the $header parameter is first. Three possible outputs: error document, XML document, or nothing; all three terminate the application.

--- system/init/common/show_error.php
+++ system/init/common/show_error.php
@@ -18,53 +18,63 @@
 		/**
 		 * Show Error
 		 *
-		 * Explain the function...
+		 * Terminate the application, and display an optional error or error document.
+		 * This function takes two parameters, both optional.
+		 * The first is the header, the combination of a status code and a status. If this string is not formatted
+		 * properly, it will default to "500 Framework Application Error". The second is the error message; if this
+		 * value is not a string, it will simply terminate the application after sending the header in the first
+		 * parameter; if it is a string, it will attempt to load an error document from
+		 * "<theme_directory>/errors/<statuscode>.php" or simply print out an XML document if one cannot be found in
+		 * that location.
 		 *
+		 * @access public
+		 * @param string|non-string $message
+		 * @param string $header
 		 * @return exit
 		 */
-		function show_error($msg, $header = '500 Framework Application Error', $user_error = false) {
-			if(is_string($header)
-				&& preg_match('|^([0-9]{3}) |', $header, $matches)
-				&& (int) $matches[1] < 600
-				&& (int) $matches[1] >= 100
-			) {
-				if(!headers_sent()) {
-					header($header, true, (int) $matches[1]);
-				}
+		function show_error($header = '', $message = false) {
+			// First parse the header from the string provided. If it is invalid, send a standard 500
+			// "the-server-screwed-up" status.
+			if(!is_string($header) || preg_match('/^([0-9]{3}) ([a-zA-Z -]+)$/', $header, $matches) || $matches[1] >= 600 || $matches[1] < 100) {
+				$matches[1] = '500';
+				$matches[2] = 'Framework Application Error';
+				$header = $matches[1] . ' ' . $matches[2];
 			}
-			else {
-				$header = '500 Framework Application Error';
+			$code = (int) $matches[1];
+			$status = trim($matches[2]);
+			// If headers have not already been sent, send the header.
+			headers_sent() || header('HTTP/1.1 ' . $header, true, $code);
+			// If the message is not a string, terminate immediately. This implies that the user does not want to send any message or error document.
+			if(!is_string($message) || !$message) {
+				exit;
 			}
-			// Grab some information from the backtrace. It might be useful.
+			// If the message was a non-empty string, carry on, starting with an array compilation of important
+			// information.
 			$trace = debug_backtrace();
-			$error = array(
-				'message'	=> $msg,
-				'title'		=> $header,
-				'status'	=> (int) $matches[1],
-				'file'		=> $trace[0]['file'],
-				'line'		=> $trace[0]['line']
+			$information = array(
+				'title'			=> $status,
+				'statuscode'	=> $code,
+				'message'		=> $message,
+				'file'			=> $trace[0]['file'],
+				'line'			=> $trace[0]['line'],
 			);
-			if(is_array($user_error)) {
-				foreach($user_error as $overwrite => $value) {
-					if(isset($error[$overwrite])) {
-						$error[$overwrite] = $value;
-					}
-				}
+			// Try to load an error document view. This will be located in the "errors" theme directory.
+			$file = theme_path('errors') . $code . EXT;
+			if(file_exists($file)) {
+				headers_sent() || header('Content-Type: text/html');
+				// Before loading the error document view, unset any variable we do not want in there.
+				${'1file'} = $file;
+				unset($header, $matches, $code, $status, $message, $trace, $file);
+				// Extract the error information.
+				extract($information);
+				// Load in the error document view, like a regular view, except we don't use any output buffering.
+				require $file;
 			}
-			if(file_exists(theme_path('errors') . 'error' . EXT)) {
-				extract($error);
-				// Unset any variables that we don't want included in the error document.
-				unset($msg, $header, $matches, $trace, $error);
-				// We are writing about the path twice because we don't want to set
-				// anymore variables.
-				require theme_path('errors') . 'error' . EXT;
-			}
+			// Else just print out the error in XML format, with a text/xml header.
 			else {
-				// No HTML document to show? Dump out the data in an XML document
-				// instead.
 				echo '<?xml version="1.0" encoding="utf-8" ?>' . "\n<error>\n";
-				foreach($error as $element => $value) {
-					echo "  <{$element}>\n    {$value}\n  </{$element}>\n";
+				foreach($information as $element => $value) {
+					echo "\t<{$element}>{$value}</{$element}>\n";
 				}
 				echo '</error>';
 			}
