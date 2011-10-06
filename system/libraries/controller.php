2ea66a9b20137fc0ad278bd5bb3e3ce1aecc2a50
Zander Baldwin <mynameiszanders@gmail.com>
2011-10-06 18:14:01 +0100

    Rewrote the controller library to combine the model() and setModel(), and the module() and setModule() methods. Also completed the _autoload() method.

--- system/libraries/controller.php
+++ system/libraries/controller.php
@@ -22,17 +22,14 @@
 	 * Eventing Controller Class
 	 */
 	class controller extends library {
-
-		// We don't want to create an extra instance when extending classes, so store an instance of this class in the
-		// following variable.
+		// We don't want to create an extra instance when extending classes, so store the first instance of this class in the following variable.
 		protected static $_instance;
-		private $loader = 'load';
-
-		private static $models = array(),
-			$modules = array();
-
+		// Create containers for
+		private static $_modules = array(),
+			$_models = array();
+		
 		/**
-		 * Controller Construct Function
+		 * Controller constructor method
 		 *
 		 * @access protected
 		 * @return void
@@ -42,34 +39,29 @@
 			if(!is_object(self::$_instance)) {
 				self::$_instance =& $this;
 			}
-			// Initialise the Framework's core loading class.
-			$loadobj = load_class($this->loader);
+			// Define the Loader class name here, so we can reduce the amount of hard-coded referencing.
+			$loader = 'load';
+			// Initialise the frameworks core loading class.
+			$loadobj = load_class($loader);
 			if(!is_object($loadobj)) {
-				show_error(
-					'',
-					'Framework application dependancy "Loader" class does not exist.'
-				);
+				show_error('', 'Framework application dependacny "Load" class does not exist.');
 			}
 			// Set the Framework core loader to a property of the super object. If the
 			// loader property has already been set by something else that is
 			// unusable, overwrite it.
-			if(!isset($this->{$this->loader}) || !method_exists($this->{$this->loader}, 'library')) {
-				$this->{$this->loader} = $loadobj;
+			if(!isset($this->$loader) || !is_a($this->$loader, ns(NS, NSLIBRARY) . $loader)) {
+				$this->$loader = $loadobj;
 			}
-			// Run one final check on the loader class itself, before attempting to use
-			// it.
-			if(!method_exists($this->{$this->loader}, 'library')) {
-				show_error(
-					'',
-					'Framework application dependancy "Loader" class missing library method.'
-				);
+			// Run one final check on the loader class itself before attempting to use it.
+			if(!method_exists($this->$loader, 'library')) {
+				show_error('Framework application dependancy "Load" class missing required "library()" method.');
 			}
 			// Define the core minimal required libraries that the application needs to run.
 			$libs = array('router', 'input', 'output');
 			// Loop through all the core required libraries and load them using the
 			// Framework Loader we just set.
 			foreach($libs as $name => $library) {
-				$this->{$this->loader}->library($library, $name);
+				$this->$loader->library($library);
 			}
 			SKELETON || $this->_autoload();
 		}
@@ -84,86 +76,87 @@
 		 */
 		private function _autoload() {
 			$autoload = get_config('autoload');
+			if(!is_array($autoload)) {
+				return false;
+			}
+			foreach($autoload as $type => $resource) {
+				if(method_exists($this->load, $type)) {
+					$resource = (array) $resource;
+					foreach($resource as $parameter) {
+						if(is_string($parameter)) {
+							$this->load->$type($parameter);
+						}
+					}
+				}
+			}
 		}
 
 		/**
-		 * Set Model
-		 * Set a model into the super models array, ready to be accessed via
-		 * controllers, views, etc.
+		 * Get or Set Model
 		 *
-		 * @static
+		 * Get or set a model with the given name. Saves to a private static array to be accessed via controllers,
+		 * views, etc.
+		 *
 		 * @access public
 		 * @param string $name
 		 * @param object $model
 		 * @param boolean $overwrite
-		 * @return boolean
+		 * @return object|boolean
 		 */
-		public static function setModel($name, $model, $overwrite = false) {
-			if(!is_string($name)
-				|| !preg_match('#^' . VALIDLABEL . '$#', $name)
-				|| !is_a($model, ns(NS, NSLIBRARY) . 'model')
-			) {
+		public function model($name, $model = false, $overwrite = false) {
+			// If we do not have a valid model name, there is no point continuing, regardless of whether a model is
+			// being fetched or saved.
+			if(!is_string($name) || !preg_match('/^' . VALIDLABEL . '$/', $name)) {
 				return false;
 			}
-			if(isset(self::$models[$name]) && !$overwrite) {
-				return false;
+			// Set a model
+			if(is_a($model, ns(NS, NSLIBRARY) . 'model')) {
+				if(is_a($model, ns(NS, NSLIBRARY) . 'model') && (!isset(self::$_models[$name]) || $overwrite)) {
+					self::$_models[$name] = $model;
+				}
 			}
-			self::$models[$name] = $model;
-			return true;
+			// Get a model.
+			else {
+				if(isset(self::$_models[$name])) {
+					return self:: $_models[$name];
+				}
+			}
+			// Something failed, return bool(false).
+			return false;
 		}
 
 		/**
-		 * Model
-		 * Return an instance of a model.
+		 * Get or Set Model
 		 *
-		 * @access protected
-		 * @param string $model
-		 * @return false|object
-		 */
-		protected function model($model) {
-			return isset(self::$models[$model])
-				? self::$models[$model]
-				: false;
-		}
-
-		/**
-		 * Set Module
-		 * Set a model into the super modules array, ready to be accessed via
-		 * controllers, models, etc.
+		 * Get or set a model with the given name. Saves to a private static array to be accessed via controllers,
+		 * models, etc.
 		 *
-		 * @static
 		 * @access public
 		 * @param string $name
-		 * @param object $module
+		 * @param object $model
 		 * @param boolean $overwrite
-		 * @return boolean
+		 * @return object|boolean
 		 */
-		public static function setModule($name, $module, $overwrite = false) {
-			if(!is_string($name)
-				|| !preg_match('#^' . VALIDLABEL . '$#', $name)
-				|| !is_a($module, ns(NS, NSLIBRARY) . 'module')
-			) {
+		public function module($name, $module = false, $overwrite = false) {
+			// If we do not have a valid module name, there is no point continuing, regardless of whether a module is
+			// being fetched or saved.
+			if(!is_string($name) || !preg_match('/^' . VALIDLABEL . '$/', $name)) {
 				return false;
 			}
-			if(isset(self::$modules[$name]) && !$overwrite) {
-				return false;
+			// Set a module
+			if(is_a($module, ns(NS, NSLIBRARY) . 'module')) {
+				if(is_a($module, ns(NS, NSLIBRARY) . 'module') && (!isset(self::$_modules[$name]) || $overwrite)) {
+					self::$_modules[$name] = $module;
+				}
 			}
-			self::$modules[$name] = $module;
-			return true;
-		}
-
-		/**
-		 * Module
-		 * Return an instance of a module
-		 *
-		 * @access protected
-		 * @param string $module
-		 * @return false|object
-		 */
-		protected function module($module) {
-			return isset(self::$modules[$module])
-				? self::$modules[$module]
-				: false;
+			// Get a module.
+			else {
+				if(isset(self::$_modules[$name])) {
+					return self:: $_modules[$name];
+				}
+			}
+			// Something failed, return bool(false).
+			return false;
 		}
 
 	}
